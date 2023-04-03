37fe72e63dd2e73b343de851ebb2ddde7bee8d7e
Lin <tingyilin0820@gmail.com>
2023-04-03 22:13:45 +0800

    Order Module - 檢查User是否存在，扣除商品庫存

--- src/main/java/com/linbirdie/springbootmall/dao/ProductDao.java
+++ src/main/java/com/linbirdie/springbootmall/dao/ProductDao.java
@@ -6,6 +6,10 @@ import com.linbirdie.springbootmall.model.Product;
 
 import java.util.List;
 
+
+//隨著開發過程，可以發現Interface 裡的方法越來越多
+//假設今天剛加入一個新專案，就可以從Interface 查看有哪些方法已經被撰寫，
+//找到欲使用的方法後，再到Implement 查看細節即可
 public interface ProductDao {
 
     Integer countProduct(ProductQueryParams productQueryParams);
@@ -19,5 +23,7 @@ public interface ProductDao {
     void updateProduct(Integer productId, ProductRequest productRequest);
 
     void deleteProductById(Integer productId);
+
+    void updateStock(Integer productId, Integer stock);
 
 }
--- src/main/java/com/linbirdie/springbootmall/service/impl/OrderServiceImpl.java
+++ src/main/java/com/linbirdie/springbootmall/service/impl/OrderServiceImpl.java
@@ -2,26 +2,42 @@ package com.linbirdie.springbootmall.service.impl;
 
 import com.linbirdie.springbootmall.dao.OrderDao;
 import com.linbirdie.springbootmall.dao.ProductDao;
+import com.linbirdie.springbootmall.dao.UserDao;
 import com.linbirdie.springbootmall.dto.BuyItem;
 import com.linbirdie.springbootmall.dto.CreateOrderRequest;
 import com.linbirdie.springbootmall.model.Order;
 import com.linbirdie.springbootmall.model.OrderItem;
 import com.linbirdie.springbootmall.model.Product;
+import com.linbirdie.springbootmall.model.User;
 import com.linbirdie.springbootmall.service.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 @Component
 public class OrderServiceImpl implements OrderService {
+
+    //在Service layer 注入多個Dao滿常見
     @Autowired
     private OrderDao orderDao;
 
     @Autowired
     private ProductDao productDao;
+
+    @Autowired
+    private UserDao userDao;
+
+    //Logger 寫法很制式，如要在別的class使用，變換參數即可
+    private final static Logger log = LoggerFactory.getLogger(OrderServiceImpl.class);
 
     @Override
     public Order getOrderById(Integer orderId) {
@@ -41,11 +57,32 @@ public class OrderServiceImpl implements OrderService {
     @Override
     public Integer createOrder(Integer userId, CreateOrderRequest createOrderRequest) {
 
+        User user = userDao.getUserById(userId);
+
+        if(user == null){
+            log.warn("this userId {} is not exist", userId);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
         int totalAmount = 0;
         List<OrderItem> orderItemList = new ArrayList<>();
 
         for (BuyItem buyItem: createOrderRequest.getBuyItemList()){
             Product product = productDao.getProductByID(buyItem.getProductId());
+
+            //檢查欲購買商品product 是否存在，以及庫存是否足夠
+            if(product == null){
+                log.warn("the product {} is not exist", buyItem.getProductId());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }
+            else if(product.getStock()< buyItem.getQuantity()){
+                log.warn("the stock of product {} is not enough. The Stock is {}, but the purchase number is {}",
+                        buyItem.getProductId(), product.getStock(), buyItem.getQuantity());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }
+
+            //扣除庫存數量
+            productDao.updateStock(product.getProductID(), product.getStock() - buyItem.getQuantity());
 
             int amount = buyItem.getQuantity()*product.getPrice();
             totalAmount = totalAmount + amount;
